663215e714bb5a0f52e721f9cee3051b6f7e0abf
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2024-07-11 12:35:44 +0200

    Add  option save to PDF

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const noteArea = document.getElementById('noteArea');
 const saveButton = document.getElementById('saveButton');
 const clearButton = document.getElementById('clearButton');
+const savePdf = document.getElementById('savePdf');
 
 document.addEventListener('DOMContentLoaded', () => {
     const textArea = localStorage.getItem('note');
-    if (textArea) {
-        noteArea.value = textArea
-    }
+        if (textArea) {
+            noteArea.value = textArea
+        }
+        
+        saveButton.addEventListener('click', () => {
+            const savedText = noteArea.value;
+            localStorage.setItem('note', savedText);
+            alert('Notatka zapisana');
+        });
+
+    clearButton.addEventListener('click', () => {
+        localStorage.removeItem('note');
+        noteArea.value = '';
+        alert('Notatka została usunięta z pamięci.');
+    });
 });
 
-saveButton.addEventListener('click', () => {
-    const savedText = noteArea.value;
-    localStorage.setItem('note', savedText);
-    alert('Notatka zapisana');
+savePdf.addEventListener('click', () => {
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+    const notatka = noteArea.value;
+        
+    doc.text(notatka, 60, 10);
+    doc.save('notatka.pdf');
 });
 
-clearButton.addEventListener('click', () => {
-    localStorage.removeItem('note');
-    noteArea.value = '';
-    alert('Notatka została usunięta z pamięci.');
-})
